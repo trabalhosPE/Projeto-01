63ec8b22eacf343b9a31f21aabecf9bfbf0f16b3
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2021-09-06 19:01:44 -0300

    Update Projeto 01.c

--- Projeto 01.c
+++ Projeto 01.c
@@ -18,28 +18,96 @@ Reservar 0,'\0' e false como sinal de espaço vazio no vetor.
 Verificar se há menos de 1000 cadastros realizados quando solicitar um novo cadastro.
 Padronizar a leitura das strings(talvez fgets?).
 */
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese"); //Função que habilita a formatação em Português para todos os printfs.
     srand((int)time(0));             //Função que da a semente para o calculo da função rand().
-
+    int z, y;
     int id[MAX];
-    char nome_completo[MAX][MAX_C];
-    char email[MAX][MAX_C];
+    int menu;   // vetor de id
+    int i, contador = 0; // variaveis da validação de @
+    char nomeLogin[35];
+    char nome_completo[MAX][MAX_C]; // vetor de nome com 100 caracteres + 2
+    char email[MAX][50];            // 50 o numero de caracteres de email
     char genero[MAX][12 + 2];
     char endereco[MAX][MAX_C];
     double altura[MAX];
     bool vacina[MAX];
 
+    printf("Olá, seja bem vindo ao cadastro online da católica."); // START DO PROGRAMA
     do
     {
-        //menu
-        //ler a escolha do usuario
-        //switch
-    } while (verificaContinuidade() == true);
+        printf("\nDigite seu primeiro e ultimo nome para iniciar: "); // Validação do tamanho de nome
+        fgets(nomeLogin, 35, stdin);
+        if (strlen(nomeLogin) <= 5)
+        {
+            printf("Insira um nome válido! ");
+        }
+    } while (strlen(nomeLogin) <= 5);
 
-    return 0;
+    system("cls"); // MENU INICIAL
+    printf("-----------------\n");
+    printf("CADASTROS ONLINE \n\n");
+    printf("1.CADASTRO\n\n");
+    printf("2.EDITAR CADASTRO\n\n");
+    printf("3.EXCLUIR CADASTRO\n\n");
+    printf("4.PROCURAR CADASTRO VIA ID\n\n");
+    printf("5.PROCURAR CADASTRO VIA E-MAIL\n\n");
+    printf("6.LISTA DE CADASTROS\n\n");
+    printf("7.BACKUP DE CADASTROS\n\n");
+    printf("8.RESTAURAÇÃO(backup necessário)\n\n");
+    printf("9.SAIR\n\n");
+    scanf("%d", &menu);
+    system("cls");
+
+    switch (menu)
+    { // caso 1 : cadastros
+    case 1:
+        printf("Nome completo: ");
+        scanf("%s", &nome_completo[MAX][MAX_C]);
+        do
+        {
+            printf("Digite seu email: ");
+            fgets(email[MAX], MAX_C, stdin);
+
+            for (i = 0; i < strlen(email[MAX]); i++)
+            {
+                if ((email[MAX][i] == '@'))
+                {
+                    contador++;
+                }
+            }
+            if (contador == 1)
+            {
+                printf("Email valido.\n");
+                break;
+            }
+            else
+                printf("Email Invalido.\n");
+        } while (contador != 1);
+
+        do
+        {
+            printf("\nInforme o sexo(Feminino,Masculino ou Não Declarar): ");
+            scanf("%s", &genero[MAX]);
+            fflush(stdin);
+            if (stricmp(genero[MAX], "Feminino") && (stricmp(genero[MAX], "Masculino")) && (stricmp(genero[MAX], "Nãodeclarar"))) // Não declarar gera erro ao colocar o espaço na palavra
+                printf("\nGenero invalido.");
+        } while (strlen(genero[MAX]) < 8); // Se o sexo for menor de 7 letras ele repete. Possivel alteração: limitar apenas a 3 generos
+        printf("\nEndereço: ");
+        fgets(endereco[MAX], MAX_C, stdin);
+        do
+        {
+            printf("Altura: ");
+            scanf("%lf", &altura[MAX]);
+            if (altura[MAX] < 1 || altura[MAX] > 2)
+                printf("Altura invalida.\n");
+            fflush(stdin);
+        } while (altura[MAX] < 1 || altura[MAX > 2]);
+    }
 }
+
 void preencheId(int *id, int posicao_cont)
 {
     int numero_randomico;
